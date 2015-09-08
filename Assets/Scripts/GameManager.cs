247ce31f27788fa5676399608ef320cec65f7d96
Dan12 <dantech88@yahoo.com>
2015-09-07 22:13:06 -0500

    Init Commit. Game Stage Setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject platform;
+	public float currentSpeed = 5f;
+	public float acceleration = 0.1f;
+	public int platformView = 10;
+
+	public static GameManager instance = null;
+
+	private Vector3 gameSpeed;
+	private GameObject[] platforms;
+	private bool gameStarted = false;
+	
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		SetupGame ();
+	}
+
+	private void SetupGame(){
+		gameSpeed = new Vector3 (0f,0f,0f);
+		
+		Instantiate (player, transform.position+(new Vector3(0f,1f,0f)), Quaternion.identity);
+
+		for (int i = 0; i < platformView; i++) {
+			Instantiate (platform, transform.position + (new Vector3 (0f, -0.5f, 7f+4*i)), Quaternion.identity);
+		}
+
+		platforms = new GameObject[10];
+	}
+
+	public Vector3 getGameSpeed(){
+		return gameSpeed;
+	}
+
+	void Update(){
+		if (Input.GetButtonDown ("Fire1") && !gameStarted) {
+			gameStarted = true;
+			gameSpeed.z = 0.1f;
+		}
+
+		if (gameStarted) {
+			if(gameSpeed.z < currentSpeed)
+				gameSpeed.z = gameSpeed.z*(1f+acceleration);
+			if(gameSpeed.z > currentSpeed)
+				gameSpeed.z = currentSpeed;
+		}
+
+		print (gameSpeed);
+	}
+}
---
+++ Assets/Scripts/StartPlane.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position-GameManager.instance.getGameSpeed();
+	}
+}
